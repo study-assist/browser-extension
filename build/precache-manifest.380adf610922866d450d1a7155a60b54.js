45f32a09406784029b075b402512c79546fede63
Bleda <ungoot@hotmail.com>
2019-05-07 11:13:49 +0200

    app-built

---
+++ build/precache-manifest.380adf610922866d450d1a7155a60b54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de1cbc0c376595da55164fea2ee0876",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a27d641b28af93a78c4",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3a27d641b28af93a78c4",
+    "url": "/static/js/main.28647029.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
