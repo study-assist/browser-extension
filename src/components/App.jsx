4afbb091aff1c61d081995d6336fd0605f306031
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-05-08 12:59:08 +0200

    html structure refractor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import "./../css/App.css";
 import BookmarkTitle from "./BookmarkTitle";
 import BookmarkTag from "./BookmarkTag";
+import BookmarkNewTag from "./BoomkarkNewTag";
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
       pageTitle: "I'm currently visiting this page",
-      tags: ["fun", "notfun"]
+      tags: ["fun", "notfun", "cats"]
     };
   }
 
@@ -16,20 +17,36 @@ class App extends Component {
     this.setState({ pageTitle: title });
   };
 
+  addTag = name => {
+    this.setState(state => {
+      state.tags = [...state.tags, name];
+      return state;
+    });
+  };
+
   render() {
     return (
       <div className="">
-        <header className="">
-          <BookmarkTitle
-            title={this.state.pageTitle}
-            setPageTitle={this.setPageTitle}
-          />
-          <div className="tags">
-            {this.state.tags.map((tagname, i) => (
-              <BookmarkTag key={`tag-${i}`} tagname={tagname} />
-            ))}
-          </div>
-        </header>
+        <header className="" />
+        {/* make header a component */}
+        <main>
+          <section className="bookmark">
+            <BookmarkTitle
+              title={this.state.pageTitle}
+              setPageTitle={this.setPageTitle}
+            />
+            <ul className="tags-list">
+              {this.state.tags.map((tagname, i) => (
+                <li className={`tag-${i}`} key={`tag-${i}`}>
+                  <BookmarkTag tagname={tagname} />
+                </li>
+              ))}
+              <li className="new-tag">
+                <BookmarkNewTag addTag={this.addTag} />
+              </li>
+            </ul>
+          </section>
+        </main>
       </div>
     );
   }
