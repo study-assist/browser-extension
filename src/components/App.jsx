c4ff0a9d27a545a0a8d5cca2c7044abe22f870fa
Bleda <ungoot@hotmail.com>
2019-05-08 14:02:44 +0200

    iframe-placeholder-for-bookmarks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,6 +29,7 @@ class App extends Component {
               <BookmarkTag tagname={tagname} />
             ))}
           </div>
+          <iframe id="bookmarks" title="bookmarks" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2427.5354032226264!2d13.483447915331547!3d52.523746543704696!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a84e9ab1163a17%3A0x700b4ec74a47a34d!2sDCI+Digital+Career+Institute+GmbH+(Devugees)!5e0!3m2!1sen!2sus!4v1557316676198!5m2!1sen!2sus" width="300" height="300"></iframe>
         </header>
       </div>
     );
