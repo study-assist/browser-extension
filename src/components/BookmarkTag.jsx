5d5d16297c7ab0f2ba99a6f56dfd87add5d062c2
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-05-08 12:07:35 +0200

    fix eslintrc config

--- src/components/BookmarkTag.jsx
+++ src/components/BookmarkTag.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import propTypes from "prop-types";
 
 function BookmarkTag({ tagname }) {
   return <div className="tag">{tagname}</div>;
 }
+
+BookmarkTag.propTypes = {
+  tagname: propTypes.string
+};
+
 export default BookmarkTag;
