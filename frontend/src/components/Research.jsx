f20714c72218b3340fa9a3e8237009f204794a96
Elisabeth Vonesch <vonesch.elisabeth@gmail.com>
2019-05-13 14:15:43 +0200

    Research component skeleton

---
+++ frontend/src/components/Research.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import propTypes from "prop-types";
+
+import "../css/Research.css";
+
+import ResearchTitle from "./ResearchTitle";
+
+function Research() {
+  return <ResearchTitle />;
+}
+
+export default Research;
