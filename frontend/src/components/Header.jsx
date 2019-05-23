6674cae37cacd1739e161da8ad8da6c4dd3b7816
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-05-23 15:04:20 +0200

    add & integrate extension logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import propTypes from "prop-types";
 
+import logo from "../data/open-book.svg";
+
 const Header = ({ title }) => {
   return (
-    <header>
-      <div className="row justify-content-between no-gutters">
-        <h1 className="px-2 text-muted">{title}</h1>
-        <div className="options">
+    <header className="header">
+      <div className="row justify-content-between align-items-end no-gutters">
+        <div className="head row flex-nowrap no-gutters align-items-end">
+          <img className="logo mr-2" src={logo} alt="Study Assist logo" />
+          <h1 className="mb-0">{title}</h1>
+        </div>
+        <div className="options row justify-content-end">
           <i className="fas fa-share-alt" />
           <i className="fas fa-cog" />
         </div>
