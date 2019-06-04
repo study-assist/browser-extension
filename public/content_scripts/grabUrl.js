6ea26df19f4a8b9c47fe282b253b6d9e8a02c544
Elisabeth Vonesch <vonesch.elisabeth@gmail.com>
2019-06-04 14:16:10 +0200

    grabUrl and pass to popup

---
+++ public/content_scripts/grabUrl.js
@@ -0,0 +1,14 @@
+function handleResponse(message) {
+  console.log(`Message from Popup: ${message.response}`);
+}
+
+function handleError(error) {
+  console.log({ error });
+}
+
+function sendUrl(e) {
+  const sending = browser.runtime.sendMessage(window.location.href);
+  sending.then(handleResponse, handleError);
+}
+
+sendUrl();
