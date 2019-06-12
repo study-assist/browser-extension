62a4ebe177119714bf48cc6749da03296560742f
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-06-12 14:03:53 +0200

    try-outs with background.js setup

--- public/background.js
+++ public/background.js
@@ -2,10 +2,11 @@
 /* eslint-disable no-console */
 /* eslint-disable no-undef */
 
+if (browser === "undefined") return;
 // const browser = browser || chrome;
 
 const extUrl = browser.extension.getURL("");
-console.log(extUrl);
+// console.log(extUrl);
 
 let url = null;
 
@@ -13,6 +14,16 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
   url = message;
   sendResponse({ response: "Response from background.js" });
 });
+
+// const onGot = tabInfo => {
+//   console.log(tabInfo);
+// };
+
+// const onError = error => {
+//   console.log(`Error: ${error}`);
+// };
+
+// browser.tabs.getCurrent().then(onGot, onError);
 
 browser.tabs
   .executeScript({ file: "/lib/getActiveUrl.js" })
@@ -22,4 +33,4 @@ function reportExecuteScriptError(error) {
   console.error(`Failed to execute content script: ${error.message}`);
 }
 
-export default url;
+// export default url;
